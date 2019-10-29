7d9895fee5afe6f438f8d4e4aca5c2e13cb7212d
silvakelly212 <silvakelly212@gmail.com>
2019-10-29 12:31:36 -0300

    Update pesquisa_star_test.js

--- pesquisa_star_test.js
+++ pesquisa_star_test.js
@@ -4,15 +4,15 @@ Feature('validar resultado');
 Scenario('test something', (I) => {
     I.amOnPage('/');
     I.see('https://swapi.co/api/people/1/');
-    I.see('200');
-    I.see('Luke Skywalker');
-    I.see('172');
-    I.see('77');
-    I.see('blond');
-    I.see('fair');
-    I.see('blue');
-    I.see('19BBY');
-    I.see('male');
+    I.see(code,'200');
+    I.see(name,'Luke Skywalker');
+    I.see(height,'172');
+    I.see(mass,'77');
+    I.see(hair_collor,'blond');
+    I.see(skin_color,'fair');
+    I.see(eye_color,'blue');
+    I.see(birth_year,'19BBY');
+    I.see(gender,'male');
 
 });
 
@@ -21,17 +21,16 @@ Feature('validar resultado');
 Scenario('test something', (I) => {
     I.amOnPage('/');
     I.see('https://swapi.co/api/planests/1/');
-    I.see('200');
-    I.see('Tatooine');
-    I.see('23');
-    I.see('304');
-    I.see('10465');
-    I.see('arid');
-    I.see ('1 standard');
-    I.see('desert');
-    I.see('1');
-    I.see('200000');
+    I.see(code,'200');
+    I.see(name, 'Tatooine');
+    I.see(ratation, '23');
+    I.see(orbital_period,'304');
+    I.see(diameter,'10465');
+    I.see(climate,'arid');
+    I.see(gravity,'1 standard');
+    I.see(terrain,'desert');
+    I.see(surface_water,'1');
+    I.see(population,'200000')
 
 });
-
 
