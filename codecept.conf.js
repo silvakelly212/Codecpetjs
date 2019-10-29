d6460fbc09a92eddb33b7b7e049ea8bb9f73cfd2
silvakelly212 <silvakelly212@gmail.com>
2019-10-29 12:28:18 -0300

    Update codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -15,3 +15,21 @@ exports.config = {
   name: 'cmder',
   translation: 'pt-BR'
 }
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'https://swapi.co/api/planets/1/',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: 'yes'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'cmder',
+  translation: 'pt-BR'
+}
